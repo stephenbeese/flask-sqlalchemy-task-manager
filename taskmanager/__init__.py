e69e3f373153b18eb1084e866355d0a9513fac95
stephenbeese <stephenbeese1@live.co.uk>
2023-04-13 13:37:19 +0000

    Fix typo

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,7 +7,7 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-if os.eviron.get("DEVELOPMENT") == "TRUE":
+if os.environ.get("DEVELOPMENT") == "TRUE":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
